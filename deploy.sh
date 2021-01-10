ca06b935039d01a421b6ccb77927ec7baa8cb0f9
avishekmndl <mondal.avishekri@gmail.com>
2021-01-10 22:45:37 +0000

    finally deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t amndl/multi-client -f ./client/Dockerfile ./client
-docker build -t amndl/multi-server -f ./server/Dockerfile ./server
-docker build -t amndl/multi-worker -f ./worker/Dockerfile ./worker
-docker push amndl/multi-client
-docker push amndl/multi-server
-docker push amndl/multi-worker
+docker build -t amndl/multi-client:latest -t amndl/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t amndl/multi-server:latest -t amndl/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t amndl/multi-worker:latest -t amndl/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push amndl/multi-client:latest
+docker push amndl/multi-server:latest
+docker push amndl/multi-worker:latest
+
+docker push amndl/multi-client:$SHA
+docker push amndl/multi-server:$SHA
+docker push amndl/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=amndl/multi-server
+kubectl set image deployments/server-deployment server=amndl/multi-server:$SHA
+kubectl set image deployments/client-deployment client=amndl/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=amndl/multi-worker:$SHA
